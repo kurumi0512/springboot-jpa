40c4ad09b561c76cc02371cba8ddb54c741ceef5
kurumi0512 <foreverlove0512t@gmail.com>
2025-05-19 15:05:50 +0800

    250519 15:05

--- src/main/java/com/example/demo/controller/RoomController.java
+++ src/main/java/com/example/demo/controller/RoomController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.dto.RoomDto;
@@ -19,10 +21,10 @@ import jakarta.validation.Valid;
 // Method URI            功能
 // --------------------------------------------------------------------
 // GET    /rooms                查詢所有會議室(多筆)
-// GET    /room/{roomId}        查詢指定會議室(單筆)
+// GET    /room/{roomId}        查詢指定會議室(單筆)-修改專用
 // POST   /room                 新增會議室
-// POST   /room/update/{roomId} 完整修改會議室(同時修改 roomName 與 roomSize)
-//GET    /room/delete/{roomId} 刪除會議室
+// PUT   /room/update/{roomId} 完整修改會議室(同時修改 roomName 與 roomSize)
+//DELETE    /room/delete/{roomId} 刪除會議室
 //--------------------------------------------------------------------
 
 @Controller
@@ -41,12 +43,12 @@ public class RoomController {
 		return "room/room";
 	}
 
-	/*
-	 * @GetMapping // 第二種寫法,都寫明(第一種方法是簡化) public String getRooms2(Model model) {
-	 * RoomDto roomDto = new RoomDto(); List<RoomDto> roomDtos =
-	 * roomService.findAllRooms(); model.addAttribute("roomDto", roomDto);
-	 * model.addAttribute("roomDtos", roomDtos); return "room/room"; }
-	 */
+	@GetMapping("/{roomId}")
+	public String getRoom(@PathVariable Integer roomId, Model model) {
+		RoomDto roomDto = roomService.getRoomById(roomId);
+		model.addAttribute("roomDto", roomDto);
+		return "room/room_update";
+	}
 
 	// @Valid RoomDto roomDto, BindingResult bindingResult
 	// RoomDto 要進行屬性資料驗證, 驗證結果放到 bindingResult
@@ -61,6 +63,18 @@ public class RoomController {
 
 		// 進行新增
 		roomService.addRoom(roomDto);
+		return "redirect:/rooms";
+	}
+
+	@PutMapping("/update/{roomId}")
+	public String updateRoom(@PathVariable Integer roomId, @Valid RoomDto roomDto, BindingResult bindingResult) {
+		// 驗證資料
+		if (bindingResult.hasErrors()) { // 若驗證時有錯誤發生
+			return "room/room_update";
+		}
+
+		// 進行修改
+		roomService.updateRoom(roomId, roomDto);
 		return "redirect:/rooms";
 	}
 
--- src/main/java/com/example/demo/model/dto/RoomDto.java
+++ src/main/java/com/example/demo/model/dto/RoomDto.java
@@ -2,6 +2,7 @@ package com.example.demo.model.dto;
 
 import org.hibernate.validator.constraints.Range;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,16 +12,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class RoomDto {
-	@NotNull(message = "房號不可以是空值") // 如果是數字範圍要用range
-	@Range(min = 1, max = 9999, message = "房號必須介於 {min} ~ {max} 之間")
+public class RoomDto { // 數字範圍要用range
+	@NotNull(message = "{roomDto.roomId.notNull}")
+	@Range(min = 1, max = 9999, message = "{roomDto.roomId.range}")
 	private Integer roomId;
 
-	@NotNull(message = "房名不可以是空值")
-	@Size(min = 2, message = "房名至少要有 {min} 個字") // 幾個字的話要用size
+	@NotEmpty(message = "{roomDto.roomName.notEmpty}")
+	@Size(min = 2, message = "{roomDto.roomName.size}")
 	private String roomName;
 
-	@NotNull(message = "房間人數不可以是空值")
-	@Range(min = 1, max = 500, message = "房間人數必須介於 {min} ~ {max} 之間")
+	@NotNull(message = "{roomDto.roomSize.notNull}")
+	@Range(min = 1, max = 500, message = "{roomDto.roomSize.range}")
 	private Integer roomSize;
 }
