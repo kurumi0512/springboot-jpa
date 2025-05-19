b2cf18c164cca220485fc9231c6d2ecefe81589d
kurumi0512 <foreverlove0512t@gmail.com>
2025-05-19 16:55:35 +0800

    250519 16:55

--- src/main/java/com/example/demo/controller/RoomRestController.java
+++ src/main/java/com/example/demo/controller/RoomRestController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,13 +30,14 @@ import com.example.demo.service.RoomService;
 //
 
 @RestController
-@RequestMapping("/rest/room")
+@RequestMapping(value = { "/rest/room", "/rest/rooms" })
 @CrossOrigin(origins = { "http://localhost:5173", "http://localhost:8002" }, allowCredentials = "true")
 public class RoomRestController {
 
 	@Autowired
 	private RoomService roomService;
 
+	// 取得所有房間列表
 	@GetMapping
 	public ResponseEntity<ApiResponse<List<RoomDto>>> findAllRooms() {
 		List<RoomDto> roomDtos = roomService.findAllRooms(); // payload
@@ -41,25 +45,38 @@ public class RoomRestController {
 		return ResponseEntity.ok(ApiResponse.success(message, roomDtos));
 	}
 
-	// 取得指定房間資料
-	@GetMapping("{roomId}")
-	public ResponseEntity<ApiResponse<RoomDto>> findRoomById(@PathVariable Integer roomId) {
-		RoomDto roomDto = roomService.getRoomById(roomId);
-		if (roomDto == null) {
-			return ResponseEntity.ok(ApiResponse.success("查無資料", null));
-		}
-		return ResponseEntity.ok(ApiResponse.success("查詢成功", roomDto));
+	// 新增房間
+	@PostMapping
+	public ResponseEntity<ApiResponse<RoomDto>> addRoom(@RequestBody RoomDto roomDto) {
+		roomService.addRoom(roomDto);
+		return ResponseEntity.ok(ApiResponse.success("Room 新增成功", roomDto));
 	}
 
-	@DeleteMapping("{roomId}")
-	public String deleteBook(@PathVariable Integer roomId, Model model) {
-		try {
-			roomService.deleteRoom(roomId);
-		} catch (RoomException e) {
-			model.addAttribute("message", "刪除錯誤: " + e.getMessage());
-			return "error";
-		}
-		return "redirect:/rest/room";
+	// 取得單筆
+	@GetMapping("/{roomId}")
+	public ResponseEntity<ApiResponse<RoomDto>> getRoom(@PathVariable Integer roomId) {
+		RoomDto roomDto = roomService.getRoomById(roomId);
+		return ResponseEntity.ok(ApiResponse.success("Room 查詢單筆成功", roomDto));
+	}
+
+	// 修改房間
+	@PutMapping("/{roomId}")
+	public ResponseEntity<ApiResponse<RoomDto>> updateRoom(@PathVariable Integer roomId, @RequestBody RoomDto roomDto) {
+		roomService.updateRoom(roomId, roomDto);
+		return ResponseEntity.ok(ApiResponse.success("Room 修改成功", roomDto));
+	}
+
+	// 刪除房間
+	@DeleteMapping("/{roomId}")
+	public ResponseEntity<ApiResponse<Integer>> deleteRoom(@PathVariable Integer roomId) {
+		roomService.deleteRoom(roomId);
+		return ResponseEntity.ok(ApiResponse.success("Room 刪除成功", roomId));
+	}
+
+	// 錯誤處理
+	@ExceptionHandler({ RoomException.class })
+	public ResponseEntity<ApiResponse<Void>> handleRoomExceptions(RoomException e) {
+		return ResponseEntity.status(500).body(ApiResponse.error(500, e.getMessage()));
 	}
 
 }
