2e1a032eaf95993e39ebdf8dd25d26f813031619
kurumimi <foreverlove0512t@gmail.com>
2025-05-23 22:02:48 +0800

    250523 22:03

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -23,9 +23,11 @@ public class LoginController {
 
 	@GetMapping
 	public String loginPage() {
-		return "login";
+		return "login"; // 回傳 login.jsp 頁面
 	}
 
+	// Model 是傳資料給 JSP 用的
+	// HttpSession 是儲存登入狀態的容器
 	@PostMapping
 	public String checkLogin(@RequestParam String username, @RequestParam String password, Model model,
 			HttpSession session) {
@@ -34,13 +36,18 @@ public class LoginController {
 		try {
 			userCert = certService.getCert(username, password);
 		} catch (CertException e) {
-			session.invalidate();
+			session.invalidate();// 清掉舊的 session
 			// 將錯誤資料丟給 error.jsp
-			model.addAttribute("message", e.getMessage());
+			model.addAttribute("message", e.getMessage()); // 顯示錯誤訊息
 			e.printStackTrace();
-			return "err";
+			return "err"; // 跳轉到錯誤頁面 err.jsp
 		}
 
+//		登入成功後，把 userCert（登入憑證）存到 session
+//
+//		代表「此使用者已登入」，後續可以透過這個憑證做身份驗證
+//
+//		然後 redirect:/room 表示重導到 /room 頁面
 		// 將憑證放到 session 變數中, 以利其他程式進行取用與驗證
 		session.setAttribute("userCert", userCert);
 		return "redirect:/room"; // 重導到首頁
