c016ae7042f52e3d49f848a2c939d3f131edd9cf
kurumimi <foreverlove0512t@gmail.com>
2025-05-23 21:55:53 +0800

    250523 21:55

--- src/main/java/com/example/demo/service/impl/CertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CertServiceImpl.java
@@ -19,17 +19,19 @@ public class CertServiceImpl implements CertService {
 	@Override
 	// 這是登入的概念,登入就要拿到一個憑證
 	// public UserCert getCert(String username, String password) throws
-	// CertException {
+	// CertException { ->implements可以拋出CertService,也可以拋出子類(Usernotfound)
 	public UserCert getCert(String username, String password) throws UserNotFoundException, PasswordInvalidException {
 		// 1. 是否有此人
 		User user = userRepository.getUser(username);
 		if (user == null) {
 			throw new UserNotFoundException("查無此人");
+			// 自訂UserNotFoundException
 		}
 		// 2. 密碼 hash 比對,把password跟資料庫salt拿出來
 		String passwordHash = Hash.getHash(password, user.getSalt());
 		if (!passwordHash.equals(user.getPasswordHash())) {
 			throw new PasswordInvalidException("密碼錯誤");
+			// 自訂PasswordInvalidException
 		}
 		// 3. 簽發憑證
 		UserCert userCert = new UserCert(user.getUserId(), user.getUsername(), user.getRole());
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -19,6 +19,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserMapper userMapper;
 
+	// 從資料庫撈出 User 實體
+	// 轉換為 UserDto（用 UserMapper 或手動轉換）
+	// 回傳給前端使用（安全、不含密碼）
 	@Override
 	public UserDto getUser(String username) {
 		User user = userRepository.getUser(username);
@@ -27,6 +30,10 @@ public class UserServiceImpl implements UserService {
 		}
 		return userMapper.toDto(user);
 	}
+
+	// 寫入資料庫用，通常會：先對密碼進行加密（加 salt）
+	// 建立 User 實體（不是 UserDto）
+	// 呼叫 userRepository.save(user) 儲存資料
 
 	@Override
 	public void addUser(String username, String password, String email, Boolean active, String role) {
